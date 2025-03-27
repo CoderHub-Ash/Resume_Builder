f51eda9b7717b162372c8080f11eecec825706a7
Ashish Joshi <ashish90821@gmail.com>
2025-03-27 21:57:17 +0530

    Initial commit

---
+++ ResumeBuilder.Domain/ResumeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeBuilder.Domain
+{
+    public class ResumeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
